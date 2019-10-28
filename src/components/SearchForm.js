bdb894ed7bb800cf57c7f96020c1341b04e331c7
Kira <kawoods826@gmail.com>
2019-10-27 21:51:57 -0700

    added button to refresh page to allow new search

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 export default function SearchForm(props) {
+  function refreshPage() {
+    window.location.reload();
+  }
   return (
     <section className="search-form">
       <form onSubmit={props.handleSubmit}>
@@ -14,6 +17,9 @@ export default function SearchForm(props) {
           onChange={props.handleChange}
         />
         <button type="submit">Search</button>
+        <button type="submit" onClick={refreshPage}>
+          New Search{" "}
+        </button>
       </form>
     </section>
   );
