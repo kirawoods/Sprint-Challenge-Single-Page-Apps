634a54e2b0422929d3beae3c3d6ac717ee322d64
Kira <kawoods826@gmail.com>
2019-10-27 18:09:48 -0700

    added character names list to app.js

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,27 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
-
+  const [characters, setCharacters] = useState([]);
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const getCharacters = () => {
+      axios
+        .get("https://rickandmortyapi.com/api/character/")
+        .then(response => {
+          console.log(response);
+          setCharacters(response.data.results);
+        })
+        .catch(error => {
+          console.error("Server Error", error);
+        });
+    };
+    getCharacters();
   }, []);
-
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character => (
+        <h2 key={character.id}>{character.name}</h2>
+      ))}
     </section>
   );
 }
